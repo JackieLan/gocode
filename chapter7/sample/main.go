27f74210958070ae301c68458558b3a6607b4aa9
Jackie Lan <lanhust@gmail.com>
2021-02-14 19:49:09 +0800

    Init chapter7

---
+++ chapter7/sample/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/JackieLan/gocode/chapter7/sample/runner"
+)
+
+func main() {
+	runner.TestRunner()
+}
---
+++ chapter7/sample/runner/runner.go
@@ -0,0 +1,90 @@
+package runner
+
+import (
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func TestRunner() {
+	const timeout = 3 * time.Second
+	r := New(timeout)
+	r.Add(createTask(), createTask(), createTask())
+	if err := r.Start(); err != nil {
+		switch err {
+		case ErrTimeout:
+			log.Println("Terminating due to timeout.")
+			os.Exit(1)
+		case ErrInterrupt:
+			log.Println("Terminating due to interrupt.")
+			os.Exit(2)
+		}
+	}
+	log.Println("Process ended.")
+}
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Printf("Processor - Task #%d.", id)
+		time.Sleep(time.Duration(id) * time.Second)
+		log.Printf("Processor - Task #%d Done.", id)
+	}
+}
+
+type Runner struct {
+	interrupt chan os.Signal
+	complete  chan error
+	timeout   <-chan time.Time
+	tasks     []func(int)
+}
+
+var ErrTimeout = errors.New("Received timeout")
+var ErrInterrupt = errors.New("Received interrupt")
+
+func New(d time.Duration) *Runner {
+	return &Runner{
+		interrupt: make(chan os.Signal, 1),
+		complete:  make(chan error),
+		timeout:   time.After(d),
+	}
+}
+
+func (r *Runner) Add(tasks ...func(int)) {
+	r.tasks = append(r.tasks, tasks...)
+}
+
+func (r *Runner) Start() error {
+	signal.Notify(r.interrupt, os.Interrupt)
+	go func() {
+		r.complete <- r.run()
+	}()
+	select {
+	case err := <-r.complete:
+		log.Printf("handle <-r.complete %v", err)
+		return err
+	case <-r.timeout:
+		return ErrTimeout
+	}
+}
+
+func (r *Runner) run() error {
+	for id, task := range r.tasks {
+		if r.gotInterrupt() {
+			return ErrInterrupt
+		}
+		task(id)
+	}
+	return nil
+}
+
+func (r *Runner) gotInterrupt() bool {
+	select {
+	case <-r.interrupt:
+		signal.Stop(r.interrupt)
+		return true
+	default:
+		return false
+	}
+}
